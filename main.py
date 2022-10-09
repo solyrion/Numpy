0bc20cc82e7160318d272435ff830c3c032c4981
solyrion <sde101573@gmail.com>
2022-10-09 16:10:45 +0900

    .

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 import numpy as np
 
-# size vs len
+# 단위 행렬
+# 좌측에서 우측 아래로 대각선 방향으로 요소가 1
+# 다른 요소들은 0 / 정사각형 행렬
 
-arr1 = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(arr1.size) # 9
-print(len(arr1)) # 3
-# size = 총 요소의 개수, len = list len이랑 같은 느낌.
+# 1차 단위행렬
+arr = np.eye(1)
+print(arr) # [[1.]]
 
-#identical
-# 넘파이 요소는 모두 동일한 타입. -> 그래서 리스트보다 빠르다.
-arr = np.array([1,2,3,'a','b','c'])
-print(arr[0]) # 1 -> numpy.str 1임 숫자 X
-print(arr) # ['1' '2' '3' 'a' 'b' 'c']
-
-#중첩배열
-
-a = np.array([[1,2,3],[4,5,6]])
-print(a[1, 0]) # 4
-print(a[1, 2]) # 6
-print(a[0, 2]) # 3   a[행:열] = list에서 a[행][열]이랑 같음.
-
-# a.shape == np.shape(a)
-x = a.shape # (2, 3)
-y = a.shape[0] # 2
-z = a.shape[1] # 3
-
-# 구조 변경
-
-a = np.array([[1,2,3,4,5,6], [1,2,3,4,5,6]])
-a = a.reshape(3, 4) # 3행 4열로 재배열 -> a = 에 넣어줘야 a가 변경되는 거임. a.reshape만한다고 a변경 X
-print(a) #[[1 2 3 4]
-          #[5 6 1 2]
-          #[3 4 5 6]]
-
-arr = np.zeros((2, 2)) # (x, y) - x행y열 행렬을 0으로 초기화
+# 2차 단위행렬
+arr = np.eye(2)
 print(arr)
-arr = np.ones((2, 2)) # (x, y) - x행y열 행렬을 1로 초기화
-print(arr)
+'''
+[[1. 0.]
+ [0. 1.]]
+'''
 
-# np.zeros((x,y)) -> O np.zeros(0, 0) -> X
+# 행렬의 곱셈
+arr1 = np.eye(2)
+arr2 = np.array([[3,4], [5,6]])
+test = arr1*arr2
+print(test) # 각 index에 맞게 단순히 곱하기만 함 -> 행렬의 곱셈 X
+"""
+[[3. 0.]
+ [0. 6.]]
+"""
+
+test = np.dot(arr1, arr2)
+print(test) # 이래야 제대로 된 행렬의 곱셈.
+"""
+[[3. 4.]
+ [5. 6.]]
+"""
+
+
+
+
+
 
 
 
