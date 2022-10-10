d1f8fcee7eae652f33d20f7b78cb69d6b4484bee
solyrion <sde101573@gmail.com>
2022-10-10 20:47:04 +0900

    .

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import numpy as np
 
-# np.arange(n)
+# reshape(-1, n) / reshape(n, -1) n은 정수
 
-arr = np.arange(10)
-print(arr) # [0 1 2 3 4 5 6 7 8 9] 0~n-1까지 자동으로 numpy배열 생성
+ar = np.arange(12).reshape(3, 4)
+ar1 = ar.reshape(-1, 2)
+print(ar1) # (-1, n)일때 n에 맞게 알아서 배열을 함.
+           # size=12 기준 (-1, 2) -> (6, 2) / (-1, 3) -> (4, 3)
 
-arr = np.arange(0, 10, 3)
-print(arr) # [0 3 6 9] 기본적으로 for문과 비슷.
+ar2 = ar.reshape(3, -1)
+print(ar2) # (n, -1)이여도 n에 맞춰서 배열을 함. (3, -1) -> (3, 4)
+           # size에 맞게 n을 기준으로 맞출 수 없다면 오류발생.
+           # EX) size = 12 -> (-1, 5) -> 오류
 
-arr = np.arange(1, 5, 0.5)
-print(arr) # [1.  1.5 2.  2.5 3.  3.5 4.  4.5] 증가량이 소수도 가능.
-           # 특이한것은 4.5? (1, 5) = 1 ~ 4인데? -> 이것은 (x,y,z)중 z가 없다면 디폴트가 1임
-           # 즉 z가 있다면 y-z까지임.
+ar1 = ar.reshape(-1)
+print(ar1) # [ 0  1  2  3  4  5  6  7  8  9 10 11]
+           # ar.reshape(-1) == ar.reshape(1, -1) != ar.reshape(-1, 1)
 
-arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(arr)
+ar1 = ar.reshape(-1, -1) # 맞춰줄 기준이 없기 떄문에 오류발생
 
-arr = np.arange(1, 10)
-arr = arr.reshape((3, 3))
-print(arr) # 위의 방식보다 arange + reshape를 통해 일일이 쓸 필요없이 간단히 가능함.
+ar2 = ar.reshape(6, -45)
+print(ar2) # 구지 -1이 아니더리도 - 라면 똑같이 작동함.
 
-arr = np.arange(1, 37)
-arr = arr.reshape((6, 6))
-print(arr) # 데이터가 많아질수록 편해짐. (arr.reshape() 해도 arr이 reshape 되지 않음
-           # arr = arr.reshape() 해야됨
 
-arr = np.arange(1, 37).reshape(6, 6) # 한번에도 가능
-print(arr)
+
+
+
+
 
 
 
